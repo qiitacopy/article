633a9e39ff0f0aa9ce370d1d3eb1448e9f78097e
Yamazaki Kazuo <hantsuki-guitar0612@docomo.ne.jp>
2020-08-16 13:10:08 +0000

    タイトルカラムの追加

--- postgres/initdb/1_create_tables.sql
+++ postgres/initdb/1_create_tables.sql
@@ -2,6 +2,7 @@ create table article
 (
   id SERIAL primary key,
   username VARCHAR(50) not null,
+  title  VARCHAR(50) not null,
   text VARCHAR(255) not null,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
